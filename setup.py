a53ac9b9fd202cee9e5c20a46c3ff261fc26b7d9
grongierisc <guillaume.rongier@intersystems.com>
2021-10-08 10:35:41 +0200

    mv setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='fhir-scraper',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    scripts=['app'],                  # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['src/app'],                  # The name of your scipt, and also the command you'll be using for calling it
     python_requires='>=3.6.6',
     author="Guillaume Rongier",
     author_email="guillaume.rongier@intersystems.com",
