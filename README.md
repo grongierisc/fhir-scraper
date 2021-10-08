84c4d488d4133845953ce30abce2e8417504e84c
grongierisc <guillaume.rongier@intersystems.com>
2021-10-08 09:54:43 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # fhir-scraper
 A simple python script to scrap a FHIR repository to another one.
+
+The idea is to copy/scrap/crawl a FHIR repository.
+
+The script is written in Python 3.
+
+# How to run it
+
+You can install it quickly into a Virtual Environment. First, you need to move to the source folder and create an Virtual Environment (all commands are for Unix-based OS and may need tweaking on Windows):
+
+```shell
+cd src
+python -m venv .venv
+source .venv/bin/activate
+```
+
+On Windows without Windows Subsystem for Linux, you will need to change the last command to .venv\bin\activate.bat.
+
+These commands will create a new directory, visit it, create the virtual environment, and activate it. Then you can install the dependencies.
+
+```shell
+pip install -r requirements.txt
+```
+
+Now you can run the app.
+Before running it, you can refere to the configuration.
+
+```shell
+python app.py
+```
+
+# How to configure it
+
+Edit the app.py file to configure the variable in main.
+
+    ### Source Repository
+    urlClient = "http://localhost:52773/fhir/r4/"
+    
+    ### Source authorization key
+    source_authorization=None
+
+    ### Destination Repository
+    urlServer = "http://localhost:32783/fhir/r4/"
+    
+    ### Destination authorization
+    target_authorization=None
+
+    ### List of resource to fetch
+    ### If None, the script will fetch all available resources 
+    p_resources=None
+
+    ### Limit the number of resource to fetch
+    p_limit=100
+
+# How it works
+
+By default the algorithm will read the metadata of the source. 
+For each resource found, it will retrieve it, transform it and insert it in the target with the same ID.
+
+The transformation is done by the transform function, it is based on the keys/values of the resource. In this example, the transformation ignores the endpoint keys and transforms the id values.
+
+In case of error, the script moves to the next resource.
---
+++ src/app.py
@@ -0,0 +1,84 @@
+
+from fhirpy import SyncFHIRClient
+import requests
+import logging
+
+def main():
+    """
+    A little command line tool to crawl/scrap a FHIR repository to a new one.
+    You must specify the url of the source repository and the target repository.
+    """
+
+    ### Source Repository
+    urlClient = "http://localhost:52773/fhir/r4/"
+    ### Source authorization key
+    source_authorization=None
+
+    ### Destination Repository
+    urlServer = "http://localhost:32783/fhir/r4/"
+    ### Destination authorization
+    target_authorization=None
+
+    ### List of resource to fetch
+    p_resources=None
+
+    ### Limit the number of resource
+    p_limit=100
+
+    # Create the source instance
+    client = SyncFHIRClient(
+        urlClient,source_authorization,extra_headers={'Content-Type':'application/fhir+json'}
+    )
+
+    server = SyncFHIRClient(
+        urlServer,target_authorization,extra_headers={'Content-Type':'application/fhir+json'}
+    )
+
+    types = getResourceType(urlClient) if p_resources is None else p_resources
+
+    for type in types:
+        logging.info(type)
+        try:
+            resources = client.resources(type)
+            # limit of resources 
+            limit = 10 if p_limit is None else p_limit
+            for resource in resources.limit(limit):
+                print(resource.resource_type+'/'+resource.id)
+                serverResource = server.resource(resource.resource_type)
+                # map source to destination
+                for key, value in resource.items():
+                    # transform it
+                    key, value = transform(key,value)
+                    if key is not None:
+                        setattr(serverResource,key,value)
+                logging.info(serverResource.serialize())
+                # save it to the destination
+                serverResource.save()
+        except Exception as e:
+            logging.error(e)
+            pass
+
+## Get Resource all Resource type from a FHIR endpoint
+def getResourceType(url):
+    response = requests.get(url+"metadata")
+    metadata  = response.json()
+    resourcesType = list()
+    for value in metadata["rest"][0]["resource"]:
+        resourcesType.append(value["type"])
+    return resourcesType
+
+## Transfom source to destination if needed
+def transform(key,value):
+    ## replace id with _ to -
+    if key == 'id':
+        return key,value.replace("_","-")
+    ## remove endpoint
+    if key == 'endpoint':
+        return None,None
+    ## untouch
+    else:
+        return key,value
+
+if __name__ == '__main__':
+    main()
+
